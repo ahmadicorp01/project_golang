a895d3ba4b2c065475e6c240970281ef68f9ecbb
ahmadicorp01 <38343572+ahmadicorp01@users.noreply.github.com>
2019-12-09 08:47:45 +0700

    Version 0.0
    Percobaan Go Lang 1.1

---
+++ src/percobaan/percobaan.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstName := "Irfan"
+	fmt.Println("Halo ", firstName,"!\n")
+
+	lastName := "wick"
+	fmt.Println("nice to meet you ", lastName, "!\n")
+}
